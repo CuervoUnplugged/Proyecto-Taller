0405ef8f5dd9f70ea422dff655061cadcbc94668
samuelmarinvanegas-hash <samuelmarinvanegas@gmail.com>
2025-11-12 11:11:28 -0500

    Add files via upload

---
+++ damas.cpp
@@ -0,0 +1,12 @@
+#include "damas.h"
+#include "clear_window.h"
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <vector>
+
+const int SIZE = 10;
+char board[SIZE][SIZE];
